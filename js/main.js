dc7e7c922297b3ca8767e407906f536834e04c9e
mister-BlackJack <99205430+mister-BlackJack@users.noreply.github.com>
2022-07-31 02:25:09 +0700

    full optimizition site

--- js/main.js
+++ js/main.js
@@ -14,17 +14,26 @@
 (function () {
   const burgerItem = document.querySelector('.burger');
   const menu = document.querySelector('.header__nav');
-  const menuCloseItem = document.querySelector('.header__nav-close')
+  const menuCloseItem = document.querySelector('.header__nav-close');
+  const menuLinks = document.querySelectorAll('.header__link');
   burgerItem.addEventListener('click', () => {
     menu.classList.add('header__nav_active');
   });
   menuCloseItem.addEventListener('click', () => {
     menu.classList.remove('header__nav_active');
   });
+  if (window.innerWidth <= 767) {
+    for (let i = 0; i < menuLinks.length; i += 1) {
+      menuLinks[i].addEventListener('click', () => {
+        menu.classList.remove('header__nav_active');
+      })
+    }
+  }
 }());
 
 //scroll to anchors
 (function () {
+
   const smoothScroll = function (targetEl, duration) {
     const headerElHeight = document.querySelector('.header').clientHeight;
     let target = document.querySelector(targetEl);
@@ -41,10 +50,10 @@
 
     const animation = function (currentTime) {
       if (startTime === null) startTime = currentTime;
-      const timeElarsed = currentTime - startTime;
-      const run = ease(timeElarsed, startPosition, targetPosition, duration);
+      const timeElapsed = currentTime - startTime;
+      const run = ease(timeElapsed, startPosition, targetPosition, duration);
       window.scrollTo(0, run);
-      if (timeElarsed < duration) requestAnimationFrame(animation);
+      if (timeElapsed < duration) requestAnimationFrame(animation);
     };
     requestAnimationFrame(animation);
 
@@ -60,6 +69,4 @@
     });
   };
   scrollTo();
-
-
 }());
